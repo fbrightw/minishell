c14fd9527e8352e8b6bb840bc17d599c80bfd5ea
Reek Vinnie <rvinnie@vo-h3.msk.21-school.ru>
2021-04-16 12:41:25 +0300

    Ft_pwd function done

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -1,21 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.h                                         :+:      :+:    :+:   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvinnie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/16 11:11:29 by rvinnie           #+#    #+#             */
-/*   Updated: 2021/04/16 11:11:30 by rvinnie          ###   ########.fr       */
+/*   Created: 2021/04/16 12:22:46 by rvinnie           #+#    #+#             */
+/*   Updated: 2021/04/16 12:22:47 by rvinnie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTINS_H
-# define BUILTINS_H
+#include "builtins.h"
 
-# include "../minishell.h"
+// env -> environment variables
+// No errors in function
 
-void	ft_echo(char *str, int flag_n);
-void	ft_pwd(char	*env[]);
+void	ft_pwd(char	*env[])
+{
+	int		i;
+	char	*current_var;
 
-#endif
+	i = 0;
+	while (env[i])
+	{
+		current_var = env[i];
+		if (strncmp(current_var, "PWD", 3) == 0)
+			write(1, current_var + 4, strlen(current_var) - 4);
+		i++;
+	}
+	write(1, "\n", 1);
+}
